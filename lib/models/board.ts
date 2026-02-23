2d0038406423cebdf6761b0b5cb8c7eec35b2a42
Amaan Ansari <amaanansari790786@gmail.com>
2026-02-23 22:30:13 +0530

    added model

--- lib/models/board.ts
+++ lib/models/board.ts
@@ -1,4 +1,40 @@
-import mongoose, { Schema, Document } from "mongoose";
+// import mongoose, { Schema, Document } from "mongoose";
+
+// export interface IBoard extends Document {
+//   name: string;
+//   userId: string;
+//   columns: mongoose.Types.ObjectId[];
+//   createdAt: Date;
+//   updatedAt: Date;
+// }
+
+// const BoardSchema = new Schema<IBoard>(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     userId: {
+//       type: String,
+//       required: true,
+//       index: true,
+//     },
+//     columns: [
+//       {
+//         type: Schema.Types.ObjectId,
+//         ref: "Column",
+//       },
+//     ],
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// export default mongoose.models.Board ||
+//   mongoose.model<IBoard>("Board", BoardSchema);
+
+import mongoose, {Schema, Document} from "mongoose";
 
 export interface IBoard extends Document {
   name: string;
@@ -8,28 +44,25 @@ export interface IBoard extends Document {
   updatedAt: Date;
 }
 
-const BoardSchema = new Schema<IBoard>(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: String,
-      required: true,
-      index: true,
-    },
-    columns: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Column",
-      },
-    ],
+const BoardSchema = new Schema<IBoard>({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  userId: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  columns: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Column",
+    },
+  ]
+}, {
+  timestamps: true
+});
 
-export default mongoose.models.Board ||
-  mongoose.model<IBoard>("Board", BoardSchema);
+
+export default mongoose
