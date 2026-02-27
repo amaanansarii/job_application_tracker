1b9e4a34f5729d77b4d249ee52f3b83c03b3631b
Amaan Ansari <amaanansari790786@gmail.com>
2026-02-27 06:39:56 +0530

    added kanban icons

--- components/KanbanBoard.tsx
+++ components/KanbanBoard.tsx
@@ -1,11 +1,65 @@
 "use client"
 
+import React from "react";
 import { Board } from "../lib/models/models.types";
+import {
+    Award,
+    Calendar,
+    CheckCircle2,
+    Mic,
+    MoreHorizontal,
+    MoreVertical,
+    Trash2,
+    XCircle,
+  } from "lucide-react";
 
 interface KanbanBoardProps {
     board: Board;
     userId: string;
 }
+
+interface ColConfig {
+    color: string;
+    icon: React.ReactNode;
+  }
+
+const COLUMN_CONFIG: Array<ColConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />,
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+];
+
+function DroppableColumn
+
 export default function KanbanBoard({board, userId}: KanbanBoardProps){
-    return <></>;
+
+    const columns = board.columns;
+    return <>
+        <div>
+            <div>
+                {columns.map((col, key) => {
+                    const config = COLUMN_CONFIG[key] || {color: "bg-gray-500", icon: <Calendar className="h-4 w-4"/>};
+
+                    return <></>
+                })}
+            </div>
+        </div>
+    </>;
 }
