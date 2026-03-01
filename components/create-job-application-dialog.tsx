2144adb216176bb46c498355d119858285f8786a
Amaan Ansari <amaanansari790786@gmail.com>
2026-03-01 07:07:53 +0530

    igtm

--- components/create-job-application-dialog.tsx
+++ components/create-job-application-dialog.tsx
@@ -1,18 +1,31 @@
 "use client"
 
-import React from 'react';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
+import React, { useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button';
 import { Plus } from 'lucide-react';
 import {Label} from './ui/label'
 import {Input} from './ui/input'
+import { Textarea } from './ui/textarea';
 interface CreateJobApplicationProps {
     columnId: string;
     boardId: string;
 }
 export default function CreateJobApplicationDialog({ columnId, boardId }: CreateJobApplicationProps) {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const [formData, setFormData] = useState({
+        company: "",
+        position: "",
+        location: "",
+        notes: "",
+        salary: "",
+        jobUrl: "",
+        tags: "",
+        description: "",
+    }) 
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger>
                 <Button variant={"outline"} className='w-full mb-4 justify-start text-muted-foreground border-dashed border-2 hover:border-solid hover:bg-muted/50'>
                     <Plus className='mr-2 h-4 w-4'/>
@@ -31,47 +44,52 @@ export default function CreateJobApplicationDialog({ columnId, boardId }: Create
                         <div className='grid grid-cols gap-4'>
                             <div className='space-y-2'>
                                 <Label htmlFor="company">Company *</Label>
-                                <Input id="company" required />
+                                <Input id="company" required value={formData.company} onChange={(e) => setFormData({...formData, company: e.target.value})}/>
                             </div>
                             <div className='space-y-2'>
                                 <Label htmlFor="position">Position *</Label>
-                                <Input id="position" required />
+                                <Input id="position" required value={formData.position} onChange={(e) => setFormData({...formData, position: e.target.value})}/>
                             </div>
                         </div>
 
                         <div>
                             <div className='space-y-2'>
                                 <Label htmlFor="location">Location </Label>
-                                <Input id="location" required />
+                                <Input id="location" required value={formData.location} onChange={(e) => setFormData({...formData, location: e.target.value})}/>
                             </div>
                             <div className='space-y-2'>
                                 <Label htmlFor="salary">Salary </Label>
-                                <Input id="salary" placeholder='e.g., $100k - $200k' required />
+                                <Input id="salary" placeholder='e.g., $100k - $200k' required value={formData.salary} onChange={(e) => setFormData({...formData, salary: e.target.value})}/>
                             </div>
                         </div>
 
                         <div>
                         <div className='space-y-2'>
                                 <Label htmlFor="joburl">Job URL </Label>
-                                <Input id="joburl" placeholder='https://...' required />
+                                <Input id="joburl" placeholder='https://...' required value={formData.jobUrl} onChange={(e) => setFormData({...formData, jobUrl: e.target.value})}/>
                             </div>
 
                             <div className='space-y-2'>
                                 <Label htmlFor="tags">Tags (comma-separated) </Label>
-                                <Input id="tags" placeholder='React, Tailwind, CSS' required />
+                                <Input id="tags" placeholder='React, Tailwind, CSS' required value={formData.tags} onChange={(e) => setFormData({...formData, tags: e.target.value})}/>
                             </div>
 
                             <div className='space-y-2'>
-                                <Label htmlFor="tags">Tags (comma-separated) </Label>
-                                <Input id="tags" placeholder='React, Tailwind, CSS' required />
+                                <Label htmlFor="description">Description  </Label>
+                                <Textarea id="description" rows={3} placeholder='Brief description of the role...' required value={formData.description} onChange={(e) => setFormData({...formData, description: e.target.value})}/>
                             </div>
 
                             <div className='space-y-2'>
-                                <Label htmlFor="tags">Tags (comma-separated) </Label>
-                                <Input id="tags" placeholder='React, Tailwind, CSS' required />
+                                <Label htmlFor="notes">Notes </Label>
+                                <Textarea rows={4} id="notes" required value={formData.notes} onChange={(e) => setFormData({...formData, notes: e.target.value})}/>
                             </div>
                         </div>
                     </div>
+
+                    <DialogFooter>
+                        <Button type='button' variant={"outline"} onClick={() => setOpen(false)}>Cancel</Button>
+                        <Button type='submit'>Add Application</Button>
+                    </DialogFooter>
                 </form>
             </DialogContent>
         </Dialog>
