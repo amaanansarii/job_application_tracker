0df0c0dc7b50746dfa458be2a7114ebce28ff60b
Amaan Ansari <amaanansari790786@gmail.com>
2026-03-03 18:55:50 +0530

    igtm

--- lib/models/models.types.ts
+++ lib/models/models.types.ts
@@ -1,3 +1,18 @@
+// export interface JobApplication {
+//   _id: string;
+//   company: string;
+//   position: string;
+//   location?: string;
+//   status: string;
+//   notes?: string;
+//   salary?: string;
+//   jobUrl?: string;
+//   order: number;
+//   columnId?: string;
+//   tags?: string[];
+//   description?: string;
+// }
+
 export interface JobApplication {
   _id: string;
   company: string;
