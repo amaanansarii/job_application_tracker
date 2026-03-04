da6614d7e1a7bc3819719b8c663955fa93ae3b20
Amaan Ansari <amaanansari790786@gmail.com>
2026-03-04 15:21:37 +0530

    added server function

--- lib/actions/job-application.ts
+++ lib/actions/job-application.ts
@@ -1,5 +1,12 @@
 'use server';
 
+import { error } from "console";
+import { getSession } from "../auth/auth";
+import connectDB from "../db";
+import board from "../models/board";
+import { Board, Column, JobApplication } from "../models";
+import column from "../models/column";
+
 interface JobApplicationData{
     company: string;
     position: string;
@@ -12,6 +19,65 @@ interface JobApplicationData{
     tags?: string[];
     description?: string;
 }
-export default function createJobApplication(data: {}){
+export default async function createJobApplication(data: JobApplicationData){
+    const session = await getSession();
+    
+    if(!session?.user){
+        return {error: "Unauthorized"};
+    }
 
+    await connectDB()
+
+    const {
+        company,
+        position, 
+        location,
+        notes,
+        salary,
+        jobUrl,
+        columnId,
+        boardId,
+        tags, 
+        description
+    } = data;
+
+    if (!company || !position || !columnId || !boardId){
+        return {error: "Missing required fields"};
+    }
+
+    // verify board ownership
+
+    const board = await Board.findOne({
+        _id: boardId,
+        userId: session.user.id,
+    });
+
+    if(!board){
+        return {error: "Board not found"};
+    }
+
+    //verify  column belongs to board
+
+    const column = await Column.findOne({
+        _id: columnId,
+        boardId: boardId,;
+    })
+
+    if(!column){
+        return {error: "Column not found"};
+    }
+
+    const jobApplication = await JobApplication.create({
+        company,
+        position,
+        locations,
+        notes,
+        salary,
+        jobUrl,
+        columnId,
+        boardId,
+        tags: tags || [],
+        description,
+    })
+    return
 }
