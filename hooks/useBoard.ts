f475defef4bd329612a78a42eaa6e860da9b703b
Amaan Ansari <122281681+amaanansarii@users.noreply.github.com>
2026-03-05 22:31:26 +0530

    Add imports to useBoard.ts for state management

---
+++ hooks/useBoard.ts
@@ -0,0 +1,3 @@
+import { useEffect, useState } from "react";
+import { Board, Column, JobApplication } from "../models/models.types";
+import { updateJobApplication } from "../actions/job-applications";
