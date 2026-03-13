25cd00caf83addad5277e2b66f742715bc4f7d59
Amaan Ansari <122281681+amaanansarii@users.noreply.github.com>
2026-03-13 15:51:42 +0530

    Refactor SorableJobCard to SortableJobCard

--- components/KanbanBoard.tsx
+++ components/KanbanBoard.tsx
@@ -97,11 +97,41 @@ function DroppableColumn({column, config, boardId, sortedColumns}: {column: Colu
       )
 }
 
-function SorableJobCard({job, columns} : {job: JobApplication; columns: Column[]}){
+function SortableJobCard({
+  job,
+  columns,
+}: {
+  job: JobApplication;
+  columns: Column[];
+}) {
+  const {
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+    setNodeRef,
+  } = useSortable({
+    id: job._id,
+    data: {
+      type: "job",
+      job,
+    },
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : 1,
+  };
   return (
-  <div>
-    <JobApplicationCard job={job} columns={columns}/>
-  </div>
+    <div ref={setNodeRef} style={style}>
+      <JobApplicationCard
+        job={job}
+        columns={columns}
+        dragHandleProps={{ ...attributes, ...listeners }}
+      />
+    </div>
   );
 }
 export default function KanbanBoard({board, userId}: KanbanBoardProps){
